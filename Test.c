85078dcb4e993a586f56eaeaf4e4204ed1f0eb1e
Kamil Moszumański <mski.kamil@gmail.com>
2025-11-07 10:42:08 +0100

    Update

--- Test.c
+++ Test.c
@@ -1,21 +1,21 @@
+
 #include <stdio.h>
 #include "dtabs.h"
 
 int main()
 {
-    int rows, cols, operation;
+    int rows, cols, rows2, cols2, operation;
     scanf("%d %d", &rows, &cols);
     int **table = dynamic_table_input(rows, cols);
-    dynamic_table_output(table, rows, cols);
-    int **table2 = dynamic_table_input(rows, cols);
-    dynamic_table_output(table2, rows, cols);
-    int **table3 = create_empty(rows,cols);
+    scanf("%d %d", &rows2, &cols2);
+    int **table2 = dynamic_table_input(rows2, cols2);
+    int **table3 = create_empty(rows,cols2);
     
     scanf("%d", &operation);
     
     if(operation==1)
     {
-    	add_dtabs(table, table2, table3, rows, cols);
+    	add_dtabs(table, table2, table3, rows, cols, rows2, cols2);
     	free_dtab(table,rows);
     	free_dtab(table2,rows);
 	}    	
@@ -23,16 +23,16 @@ int main()
     {
         if(operation==2)
         {
-            mul_dtabs(table, table2, table3, rows, cols);
+            mul_dtabs(table, table2, table3, rows, cols, rows2, cols2);
         }            
         else
         {
-            sub_dtabs(table, table2, table3, rows, cols);
+            sub_dtabs(table, table2, table3, rows, cols, rows2, cols2);
         }    	    
     	free_dtab(table,rows);
     	free_dtab(table2,rows);
 	}
     	
-    dynamic_table_output(table3, rows, cols);
+    dynamic_table_output(table3, rows, cols2);
     return 0;
 }
--- dtabs.c
+++ dtabs.c
@@ -37,60 +37,82 @@ void dynamic_table_output(int **tab, int rows, int cols)
     free_dtab(tab,rows);
 }
 
-void add_dtabs(int **tab, int ** tab2,int **out_tab, int rows, int cols)
+void add_dtabs(int **tab, int ** tab2,int **out_tab, int rows, int cols,int rows2,int cols2)
 {
 	int i, j;
 
-    for (i = 0; i < rows; i++)
-    {
-    	for (j = 0; j < cols; j++)
-    	{
-    		out_tab[i][j] = tab[i][j] + tab2[i][j];
-		}            
-	}
-}
-
-void sub_dtabs(int **tab, int ** tab2,int **out_tab, int rows, int cols)
-{
-	int i, j;
-	
-	for(i = 0; i < rows;i++)
+	if (cols != cols2 || rows != rows2)
 	{
-		for(j = 0; j < cols; j++)
-		{
-			out_tab[i][j] = tab[i][j] - tab2[i][j];
-		}
-	}
-}
-
-void mul_dtabs(int **tab, int ** tab2,int **out_tab, int rows, int cols)
-{
-	int i, j, k, m;
-	
-	for(i = 0; i < rows; i++)
-	{
-		for(j = 0; j < cols; j++)
-		{
-			for(k = 0; k < rows; k++)
-	        {
-                out_tab[i][j] += (tab[i][k]*tab2[k][j]);
-		 	}
-		}
-	}
-}
-
-int **create_empty(int rows, int cols)
-{
-	int i, j;
-	
-	int **tab = (int **)malloc(rows * sizeof(int *));
-    for (i = 0; i < rows; i++)
-    {
-        tab[i] = (int *)malloc(cols * sizeof(int));
-		for (j = 0; j < cols; j++) tab[i][j] = 0;
+        printf("fatal error \n");
     }
-    
-    return tab;
+	else
+	{
+		for(i = 0; i < rows;i++)
+		{
+			for(j = 0; j < cols; j++)
+			{
+				out_tab[i][j] = tab[i][j] + tab2[i][j];
+			}
+		}
+	}
+}
+
+void sub_dtabs(int **tab, int ** tab2,int **out_tab, int rows, int cols,int rows2,int cols2)
+{
+	int i, j;
+
+	if (cols != cols2 || rows != rows2)
+	{
+        printf("fatal error \n");
+    }
+	else
+	{
+		for(i = 0; i < rows;i++)
+		{
+			for(j = 0; j < cols; j++)
+			{
+				out_tab[i][j] = tab[i][j] - tab2[i][j];
+			}
+		}
+	}
+}
+	
+
+void mul_dtabs(int **tab, int ** tab2,int **out_tab, int rows, int cols,int rows2,int cols2)
+{
+	int i, j, k, m;	
+	
+    if (cols != rows2)
+	{
+        printf("fatal error \n");
+    }
+	else
+	{
+		for(i = 0; i < rows; i++)
+		{
+			for(j = 0; j < cols; j++)
+			{
+				for(k = 0; k < rows2; k++)
+	    		{
+                	out_tab[i][j] += (tab[i][k]*tab2[k][j]);
+		 		}
+			}
+		}
+	}	
+}
+
+int **create_empty(int rows, int cols) 
+{ 
+	int i, j;
+
+	int **tab = (int **)malloc(rows * sizeof(int *)); 
+	for (i = 0; i < rows; i++) 
+	{
+		tab[i] = (int *)malloc(cols * sizeof(int));
+		for(j = 0; j < cols;j++) tab[i][j]=0;
+	}
+	
+	return tab; 
 }
 
 void free_dtab(int **tab,int rows)
