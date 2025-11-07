20686a7f5652b8df56b92e50faeab68ae0054b2a
Kamil Moszumański <mski.kamil@gmail.com>
2025-11-07 14:44:59 +0100

    Fixing

---
+++ dtabs.h
@@ -0,0 +1,44 @@
+#ifndef DTABS_H
+#define DTABS_H
+
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+int **dynamic_table_input(int rows, int cols);
+
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+void dynamic_table_output(int **table, int rows, int cols);
+
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+void add_dtabs(int **tab, int ** tab2,int **out_tab, int rows, int cols,int rows2,int cols2);
+
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+void sub_dtabs(int **tab, int ** tab2,int **out_tab, int rows, int cols,int rows2,int cols2);
+
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+void mul_dtabs(int **tab, int ** tab2,int **out_tab, int rows, int cols,int rows2,int cols2);
+
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+int **create_empty(int rows,int cols);
+
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+void trans_dtab(int **tab, int **out_tab, int rows, int cols);
+
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+void free_dtab(int **tab, int rows);
+
+#endif // DTABS_H
